8f992ebce0a44aa95bf30a1b8d7619d2e1c0a4b9
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2024-05-21 09:41:51 -0700

    Ansible lint.

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -uex
-PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install --include-deps ansible
+PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install --include-deps ansible ansible-lint
